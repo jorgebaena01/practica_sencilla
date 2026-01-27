774c3b51af9b7b7185fd4c6f2952c455c2ab8888
jorgebaena01 <jbaenam@unal.edu.co>
2026-01-26 23:19:46 -0500

    Add un miniajuste

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5]
 y = [1, 4, 9, 16, 25]
+z= []
+
+z = [xi * yi - 1 for xi, yi in zip(x, y)]
 
 plt.plot(x, y)
 plt.title("Mi primera gráfica 😎")
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.show()
+
+print(z)
