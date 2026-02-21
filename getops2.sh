ea96f8ed34b81e5678bbdfd7d45ff47ac1e5dc46
jorgebaena01 <jbaenam@unal.edu.co>
2026-02-21 00:41:18 -0500

    SUBIENDO APUNTES, NADA DE CURSO POR AHORA

--- getops2.sh
+++ getops2.sh
@@ -3,7 +3,36 @@
 #inicio creando las funciones, estas dependen de los argumentos: 1: palabra 2;nombre del archivo
 
 cuenta_palabras() { 
-	echo cuantas palabrasa hay:
-	grep -c $1 $2
+	echo "cuantas palabras de tipo $2 hay:"
+	grep -c "$2" "$1"
+}
+cuenta_no_son() {
+	echo "cuantas NO son del tipo $2:"
+	grep -vc "$2" "$1"
 }
 
+archivo=""
+palabra=""
+op1=""
+op2=""
+
+
+# los dos puntos se ponen a la derecha de las variables con inputs
+while getopts "a:b:xy" opt; do
+  case $opt in
+    a) archivo="$OPTARG" ;;
+	b) palabra="$OPTARG" ;; 
+	x) op1="positivo" ;;
+	y) op2="positivo" ;;
+  esac
+done
+
+
+if [[ "$op1" == "positivo" ]]; then
+  cuenta_palabras "$archivo" "$palabra"
+fi
+
+
+if [[ "$op2" == "positivo" ]]; then
+  cuenta_no_son "$archivo" "$palabra"
+fi
