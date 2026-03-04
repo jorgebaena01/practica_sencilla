490b2e04a3686c1e3b7d26d06f65709bf3395b54
jorgebaena01 <jbaenam@unal.edu.co>
2026-03-03 22:56:59 -0500

    se da por terminado el mini proyecto, no esta muy automatico pero es para practicar nomasssss

--- ejericiotesla/grafica.py
+++ ejericiotesla/grafica.py
@@ -1,3 +1,23 @@
+
+import pandas as pd 
+import matplotlib.pyplot as plt
+
 print(' en este archivo de python voy a graficar los valores de ensayadero2.csv')
 
+# inicialmente, le daremos nombres a las columnas
+df=pd.read_csv('ensayadero2.csv')
+df.columns = ['DATE','AVERAGE']
+df['DATE']=pd.to_datetime(df['DATE'])
 
+plt.figure(figsize=(10,5))
+
+plt.plot(df['DATE'],df['AVERAGE'], marker='o', linestyle='-', color='b')
+
+plt.title('DAILY AVERAGE PRICE OVER TIME')
+plt.xlabel('DATE')
+plt.ylabel('PRICE')
+plt.grid('True')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.savefig('graficatesla.png',dpi=300, bbox_inches='tight')
